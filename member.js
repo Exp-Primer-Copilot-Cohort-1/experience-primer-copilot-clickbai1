b24fbede3e23ccedb435c53c7bf7f6f95f2a3b5b
clickbai1 <52222175+clickbai1@users.noreply.github.com>
2023-10-25 04:21:11 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+  var skills = ['HTML', 'CSS', 'JS', 'React', 'Node'];
+  var member = {
+    name: 'Ivan',
+    age: 25,
+    skills: skills
+  };
+  console.log(member.skills[2]);
+}
